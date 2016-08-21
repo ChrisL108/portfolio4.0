0324b4b1d1b016158449e3f68f46c73dfd31a530
Christopher Lacaille <Christopher Lacaille>
2016-08-21 13:55:53 -0500

    update gulp files to build js/css

--- gulpfile.js
+++ gulpfile.js
@@ -5,32 +5,43 @@
 // grab our packages
 var gulp   = require('gulp'),
     jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
     sass = require('gulp-sass'),
     uglify = require('gulp-uglify'),
     autoprefixer = require('gulp-autoprefixer');
 
-// define the default task and add the watch task to it
+// add default watch tasks
 gulp.task('default', ['watch']);
 
-// configure the jshint task
-gulp.task('jshint', function() {
-  return gulp.src('source/javascript/**/*.js')
+// BUILD JAVASCRIPT
+// Run through js-hint
+// ~then uglify
+// ~then concat into main.js
+gulp.task('build-js', function() {
+    return gulp.src('source/js/**/*.js')
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(uglify())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('public/js'))
 });
 
-gulp.task('build-css', function() {
-    return gulp.src('source/scss/*.scss')
-    .pipe(sass())
+// BUILD CSS
+// Run through sass and log errors
+// ~then autoprefixer
+// ~then concat into styles.css
+gulp.task('build-css', function () {
+  return gulp.src('./source/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'})
+        .on('error', sass.logError))
     .pipe(autoprefixer())
-    // .pipe(uglify())
-    .pipe(gulp.dest('public/assets/stylesheets'))
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('public/assets/stylesheets'));
 });
-
-// gulp.task('build-js')
+ 
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
-  gulp.watch('source/javascript/**/*.js', ['jshint']);
+  gulp.watch('source/js/**/*.js', ['build-js']);
   gulp.watch('source/scss/**/*.scss', ['build-css']);
 });
