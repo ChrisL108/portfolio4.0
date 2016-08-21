61f468bac8da0a8e001b79a6b231aea9c1126a16
Christopher Lacaille <Christopher Lacaille>
2016-08-21 00:29:55 -0500

    add jshint and gulp-sass to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,30 @@
+
+
 /* File: gulpfile.js */
 
-// grab our gulp packages
-var gulp = require('gulp'),
-    gutil = require('gulp-util');
+// grab our packages
+var gulp   = require('gulp'),
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-sass');
 
-gulp.task('default', function() {
-    return gutil.log('Gulp is running!');
+// define the default task and add the watch task to it
+gulp.task('default', ['watch']);
+
+// configure the jshint task
+gulp.task('jshint', function() {
+  return gulp.src('source/javascript/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('build-css', function() {
+    return gulp.src('source/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public/assets/stylesheets'))
+});
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+  gulp.watch('source/javascript/**/*.js', ['jshint']);
+  gulp.watch('source/scss/**/*.scss', ['build-css']);
 });
