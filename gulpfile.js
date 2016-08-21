f3a3fa43218addcdbbf269e6039e5d7dfc631e05
Christopher Lacaille <Christopher Lacaille>
2016-08-20 23:26:54 -0500

    layout project structure

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/* File: gulpfile.js */
+
+// grab our gulp packages
+var gulp = require('gulp'),
+    gutil = require('gulp-util');
+
+gulp.task('default', function() {
+    return gutil.log('Gulp is running!');
+});
