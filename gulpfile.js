072d6b7a0b39496ddcb83ed0273ce479047037b0
Christopher Lacaille <Christopher Lacaille>
2016-08-21 01:47:07 -0500

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@
 // grab our packages
 var gulp   = require('gulp'),
     jshint = require('gulp-jshint'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    autoprefixer = require('gulp-autoprefixer');
 
 // define the default task and add the watch task to it
 gulp.task('default', ['watch']);
@@ -20,8 +22,12 @@ gulp.task('jshint', function() {
 gulp.task('build-css', function() {
     return gulp.src('source/scss/*.scss')
     .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(uglify())
     .pipe(gulp.dest('public/assets/stylesheets'))
 });
+
+// gulp.task('build-js')
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
