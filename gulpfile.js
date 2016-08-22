d4b82743b00e817d5546b55617e02c8f260e605f
ChrisL108 <chrislacaille5@gmail.com>
2016-08-21 19:52:09 -0500

    got gulp working for  jshint, concat, sass, uglify, browser-sync, autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,16 @@ var gulp   = require('gulp'),
     sass = require('gulp-sass'),
     uglify = require('gulp-uglify'),
     browserSync = require('browser-sync').create(),
-    reload = browserSync.reload,
     autoprefixer = require('gulp-autoprefixer');
+    // plumber = require('gulp-plumber');
 
 // add default watch tasks
 gulp.task('default', ['watch']);
+
+function errorLog(error) {
+    console.error.bind(error);
+    this.emit('end');
+}
 
 // BUILD JAVASCRIPT
 // Run through js-hint
@@ -21,12 +26,13 @@ gulp.task('default', ['watch']);
 // ~then concat into main.js
 gulp.task('build-js', function() {
     return gulp.src('source/js/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(uglify())
-    .pipe(concat('bundle.js'))
-    .pipe(gulp.dest('public/assets/js'))
-    .pipe(reload({stream: true}));
+        .pipe(jshint()) // jshint
+        .pipe(jshint.reporter('jshint-stylish')) // add color to jshint
+        .pipe(uglify()) //minify
+        .on('error', errorLog) //catch errors
+        .pipe(concat('bundle.js'))  // put all javascript into one file
+        .pipe(gulp.dest('public/assets/js'))
+        .pipe(browserSync.stream());
 });
 
 // BUILD CSS
@@ -34,28 +40,25 @@ gulp.task('build-js', function() {
 // ~then autoprefixer
 // ~then concat into styles.css
 gulp.task('build-css', function () {
-  return gulp.src('./source/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError)) //sass({outputStyle: 'compressed'})
-    .pipe(autoprefixer())
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('public/assets/stylesheets'))
-    .pipe(reload({stream: true}));
-});
-
-
-
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: './public/'
-        }
-    });
+    return gulp.src('./source/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError)) //sass({outputStyle: 'compressed'})
+        .pipe(autoprefixer())
+        
+        .pipe(gulp.dest('public/assets/stylesheets'))
+        .pipe(browserSync.stream());
 });
  
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
-  gulp.watch('source/js/**/*.js', ['build-js']);
-  gulp.watch('source/scss/**/*.scss', ['build-css']);
-  gulp.watch('source/*.html').on('change', browserSync.reload);
+
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch('source/js/**/*.js', ['build-js']);
+    gulp.watch('source/scss/**/*.scss', ['build-css']);
+    gulp.watch('*.html').on('change', browserSync.reload);
 });
--- public/js/main.js
+++
@@ -1,2 +0,0 @@
-console.log("THIS IS THE HEADER FILE");
-var body=document.getElementById("body");body.innerHTML="<h1>Hello World! Brought to you by Gulp...",console.log("testing"),console.log("IN JUMBOTRON");
