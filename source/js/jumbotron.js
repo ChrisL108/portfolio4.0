b358f96d5cee0284aa557e9106bd4fe2cab68aa1
Christopher Lacaille <Christopher Lacaille>
2016-08-22 04:05:02 -0500

    style jumbotron

--- source/js/jumbotron.js
+++ source/js/jumbotron.js
@@ -1,2 +1,19 @@
-var jumbotron = document.getElementById("jumbotron");
-let j = 'JUMBOTRON';
+
+
+$(window).load(function() {
+  var theWindow = $(window),
+	    $bg = $("#bg"),
+	    aspectRatio = $bg.width() / $bg.height();
+  function resizeBg() {
+    if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
+      $bg
+      .removeClass()
+		  .addClass('bgheight');
+		} else {
+      $bg
+      .removeClass()
+      .addClass('bgwidth');
+      }
+    }
+  theWindow.resize(resizeBg).trigger("resize");
+  });
