9c8a56535f01947a45da54448910e264d96aeb52
ChrisL108 <chrislacaille5@gmail.com>
2016-08-21 23:27:16 -0500

    add babel,  image-min, and browser-sync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,22 @@ var gulp   = require('gulp'),
     concat = require('gulp-concat'),
     sass = require('gulp-sass'),
     uglify = require('gulp-uglify'),
-    browserSync = require('browser-sync').create(),
-    autoprefixer = require('gulp-autoprefixer');
+    browsersync = require('browser-sync').create(),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin'),
+    babel = require('gulp-babel');
     // plumber = require('gulp-plumber');
 
 // add default watch tasks
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch', 'sync']);
+
 
 function errorLog(error) {
     console.error.bind(error);
     this.emit('end');
 }
 
-// BUILD JAVASCRIPT
+// BUILD JAVASCRIPT - task
 // Run through js-hint
 // ~then uglify
 // ~then concat into main.js
@@ -28,37 +31,57 @@ gulp.task('build-js', function() {
     return gulp.src('source/js/**/*.js')
         .pipe(jshint()) // jshint
         .pipe(jshint.reporter('jshint-stylish')) // add color to jshint
-        .pipe(uglify()) //minify
-        .on('error', errorLog) //catch errors
-        .pipe(concat('bundle.js'))  // put all javascript into one file
-        .pipe(gulp.dest('public/assets/js'))
-        .pipe(browserSync.stream());
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        // .pipe(uglify()) //minify
+        .on('error', errorLog) //catch errors in console
+        .pipe(concat('bundle.js'))  // concat javascript
+        .pipe(gulp.dest('public/js'))
+        .pipe(browsersync.stream());
 });
 
-// BUILD CSS
+// BUILD CSS - task
 // Run through sass and log errors
 // ~then autoprefixer
 // ~then concat into styles.css
 gulp.task('build-css', function () {
     return gulp.src('./source/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError)) //sass({outputStyle: 'compressed'})
-        .pipe(autoprefixer())
-        
-        .pipe(gulp.dest('public/assets/stylesheets'))
-        .pipe(browserSync.stream());
+        // SaSS 
+        .pipe(sass().on('error', sass.logError)) // sass({outputStyle: 'compressed'})
+        // AutoPrefixer
+        .pipe(autoprefixer('last 2 versions'))
+        // place in public folder
+        .pipe(gulp.dest('public/styles'))
+        // Browser Sync
+        .pipe(browsersync.stream());
 });
  
-
-// configure which files to watch and what tasks to use on file changes
-gulp.task('watch', function() {
-
-    browserSync.init({
+// Browser Sync initialization
+gulp.task('sync', function() {
+    browsersync.init({
         server: {
             baseDir: './'
         }
     });
+});
 
+// COMPRESS IMAGES - task
+gulp.task('imagemin', function() {
+    gulp.src('source/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('public/img'));
+});
+
+// WATCH FILES
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+    // watch html
+    gulp.watch('*.html').on('change', browsersync.reload);
+    // watch js
     gulp.watch('source/js/**/*.js', ['build-js']);
+    // watch scss
     gulp.watch('source/scss/**/*.scss', ['build-css']);
-    gulp.watch('*.html').on('change', browserSync.reload);
+    // watch images
+    gulp.watch('source/img/*', ['imagemin']);
 });
